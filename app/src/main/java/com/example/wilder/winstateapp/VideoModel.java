10cd3cc7c3d8a329df7da76c0b3a2e310c79960e
Clairehere <b_claire@hotmail.com>
2018-06-13 18:11:55 +0200

    couleur design item video

--- app/src/main/java/com/example/wilder/winstateapp/VideoModel.java
+++ app/src/main/java/com/example/wilder/winstateapp/VideoModel.java
@@ -7,22 +7,26 @@ public class VideoModel {
     private String description;
     private String linkVideo;
     private String contributeur;
+    private String date;
     int latitude;
     int longitude;
     int fakenew;
-
     ArrayList<String> tagList;
 
-    public VideoModel(String title, String description, String linkVideo, String contributeur, int latitude, int longitude, int fakenew, ArrayList<String> tagList) {
+
+    public VideoModel(String title, String description, String linkVideo, String contributeur, String date, int latitude, int longitude, int fakenew, ArrayList<String> tagList) {
         this.title = title;
         this.description = description;
         this.linkVideo = linkVideo;
         this.contributeur = contributeur;
+        this.date = date;
         this.latitude = latitude;
         this.longitude = longitude;
         this.fakenew = fakenew;
         this.tagList = tagList;
     }
+
+    public VideoModel(){}
 
     public String getTitle() {
         return title;
@@ -54,6 +58,14 @@ public class VideoModel {
 
     public void setContributeur(String contributeur) {
         this.contributeur = contributeur;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
     }
 
     public int getLatitude() {
