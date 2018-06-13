82a8b8500ccf9bd9711beafdf63f3dfb9bfcdbe2
crocknys <crocknys@gmail.com>
2018-06-13 22:48:08 +0200

    model article

--- app/src/main/java/com/example/wilder/winstateapp/MapsActivity.java
+++ app/src/main/java/com/example/wilder/winstateapp/MapsActivity.java
@@ -227,7 +227,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
                 } else {
 
-
                     Location eventLocation = new Location(marker.getTitle());
                     moveCameraOnUser(eventLocation);
 
